4c133b014618da3544662c3655e625f4f7a2f016
nanomesher <info@nanomesher.com>
2017-04-06 08:17:29 +0800

    Add files via upload

---
+++ WeatherHttpSender/WeatherHttpSender.ino
@@ -0,0 +1,82 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+#include <ESP8266HTTPClient.h>
+
+#define USE_SERIAL Serial
+
+ESP8266WiFiMulti WiFiMulti;
+
+float t = 0;
+float h = 0;
+
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+void setup() {
+
+    USE_SERIAL.begin(115200);
+   // USE_SERIAL.setDebugOutput(true);
+
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+    USE_SERIAL.println();
+
+    for(uint8_t t = 4; t > 0; t--) {
+        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+        USE_SERIAL.flush();
+        delay(1000);
+    }
+
+    WiFiMulti.addAP("SSID", "PASSWORD");
+
+}
+
+void loop() {
+    // wait for WiFi connection
+    if((WiFiMulti.run() == WL_CONNECTED)) {
+
+        t = sht31.readTemperature();
+        h = sht31.readHumidity();
+
+        
+        char temp[10];
+        dtostrf(t,1,1,temp);
+
+        char humid[10];
+        dtostrf(h,1,1,humid);
+        
+        String url = "http://192.168.1.12/TextSetText?p=0&n=line1&v=";
+        
+        HTTPClient http;
+
+        USE_SERIAL.print("[HTTP] begin...\n");
+        http.begin(url + temp + "c " + humid + "%"); //HTTP
+
+        USE_SERIAL.print("[HTTP] GET...\n");
+        // start connection and send HTTP header
+        int httpCode = http.GET();
+
+        // httpCode will be negative on error
+        if(httpCode > 0) {
+            // HTTP header has been send and Server response header has been handled
+            USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+            // file found at server
+            if(httpCode == HTTP_CODE_OK) {
+                String payload = http.getString();
+                USE_SERIAL.println(payload);
+            }
+        } else {
+            USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+        }
+
+        http.end();
+    }
+
+    delay(10000);
+}
+
